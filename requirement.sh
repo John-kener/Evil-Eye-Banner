b0a5deb70ffd46e055313e70803b0b36e92ba820
John Kener <73270248+John-kener@users.noreply.github.com>
2020-11-10 20:05:51 +0530

    requirement.sh

---
+++ requirement.sh
@@ -0,0 +1,21 @@
+# BY JOHN KENER @ CYBER WARRIORS
+
+pkg install python -y
+pkg install python2 -y
+pkg install figlet -y
+pkg install cowsay -y
+pkg install lolcat -y
+echo
+echo
+figlet BASIC
+figlet ITEMS
+pkg install nano -y
+pkg install ruby -y
+pkg update && pkg upgrade
+figlet DONE |lolcat
+echo -e "\e[1;32m THIS TOOL WAS MADE BY JOHN KENER @ CYBER WARRIORS"
+echo
+echo
+echo Now run :- bash Evil-banner.sh
+echo
+echo
